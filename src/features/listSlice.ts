e03bfe5f0d5f5e1c4bdd18d5cc9438d4210d5297
Santiago Pintos <spintos@outlook.es>
2024-08-16 21:03:43 -0300

    feat: Add listSlice to manage list items

---
+++ src/features/listSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Item {
+    id: number;
+}
+
+interface ListState {
+    items: Item[];
+}
+
+const initialState: ListState = {
+    items: [],
+}
+
+export const listSlice = createSlice({
+    name: "list",
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter((item: Item) => item.id !== action.payload);
+        },
+        clear : (state) => {
+            state.items = [];
+        }
+    },
+});
+
+export const { addItem, removeItem, clear } = listSlice.actions
+export default listSlice.reducer
