f52285a5c67fb5857b634bb99fee3c577369d319
Santiago Pintos <spintos@outlook.es>
2024-08-16 21:06:23 -0300

    chore: Add Redux store provider to App component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,11 @@
+import { store } from './store/store'
+import { Provider } from 'react-redux'
+
 function App(): JSX.Element {
   return (
-    <>
+    <Provider store={store}>
       <p>Hello!</p>
-    </>
+    </Provider>
   )
 }
 
