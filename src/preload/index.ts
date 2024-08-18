b9cd37fa824809981c17d51b89cfb3e769e669da
Santiago Pintos <spintos@outlook.es>
2024-08-18 20:27:07 -0300

    refactor: preload: only expose custom apis to render process

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,23 +1,15 @@
 import { contextBridge, ipcRenderer } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
 const api = {
-  sendToken: (token: string) => ipcRenderer.send('send-token', token),
+  sendToken: async (token: string) => {
+    ipcRenderer.send('send-token', token)
+  },
 }
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
-if (process.contextIsolated) {
-  try {
-    contextBridge.exposeInMainWorld('api', api)
-  } catch (error) {
-    console.error(error)
-  }
-} else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI
-  // @ts-ignore (define in dts)
-  window.api = api
+try {
+  contextBridge.exposeInMainWorld('api', api)
+} catch (error) {
+  console.error(error)
 }
+
