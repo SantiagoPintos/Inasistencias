6d905066654abf2a3a25b46dd6f8252a9e426a90
Santiago Pintos <spintos@outlook.es>
2024-08-23 20:21:07 -0300

    refactor: Add keySlice module and update store configuration

--- src/renderer/src/store/store.ts
+++ src/renderer/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { Store } from 'redux'
 import listReducer from '../features/listSlice'
+import keyReducer from '../features/keySlice'
 
 export const store: Store = configureStore({
   reducer: {
-    list: listReducer
+    list: listReducer,
+    key: keyReducer
   }
 })
