52d35615bacc0dbc9b15770a116c81810e4f0e90
Santiago Pintos <spintos@outlook.es>
2024-08-23 23:14:01 -0300

    refactor: App: Conditionally render AddToken component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,35 @@
-import { store } from './store/store'
-import { Provider } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import AddToken from './components/AddToken'
 
 function App(): JSX.Element {
+  const dispatch = useDispatch()
+  const key = useSelector((state: { key: { id: string } }) => state.key.id)
+  const [thereIsKey, setThereIsKey] = useState(false)
+
+  useEffect(() => {
+    const getKey = async () => {
+      try{
+        const getKeyFromDb = await window.api.getToken()
+        if(getKeyFromDb){
+          dispatch({ type: 'key/setKey', payload: getKeyFromDb })
+          setThereIsKey(true)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getKey()
+  }, [])
+
   return (
-    <Provider store={store}>
-      <p>Hello!</p>
-    </Provider>
+    <>
+      {thereIsKey ? (
+        null
+      ) : (
+        <AddToken />
+      )}
+    </>
   )
 }
 
