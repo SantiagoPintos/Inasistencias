bc971617e3b0891a23561bc2a9723f497c201377
Santiago Pintos <spintos@outlook.es>
2024-08-27 14:01:22 -0300

    refactor: Add error alert functionality and input validation to AddData component

--- src/renderer/src/components/AddData.tsx
+++ src/renderer/src/components/AddData.tsx
@@ -1,20 +1,38 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { Button } from './ui/Button'
 import { Input } from './ui/Input'
+import { Label } from './ui/Label'
+import { AlertCircle } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from "./ui/Alert"
 
 const AddData = () => {
     const token = useRef<HTMLInputElement>(null)
+    const sheetId = useRef<HTMLInputElement>(null)
+    const [showAlert, setShowAlert] = useState(false)
     
     const handleSubmit = async () => {
-      if(token.current?.value !== '') {
+      if(token.current?.value !== '' && sheetId.current?.value !== '') {
         await window.api.sendToken(token.current!.value)
         token.current!.value = ''
+        sheetId.current!.value = ''
+      } else {
+        setShowAlert(true)
       }
     }
 
     return (
-        <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input type="email" placeholder="Token" ref={token}/>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          {showAlert && (
+            <Alert variant="destructive" className="mb-5">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>Datos incorrectos</AlertDescription>
+            </Alert>
+          )}
+          <Label htmlFor="token">API Key</Label>
+          <Input type="text" id="token" placeholder="API Key" className='mb-5' ref={token}/>
+          <Label htmlFor="sheet" >URL de Google Sheets</Label>
+          <Input type="text" id="sheet" placeholder="https://docs.google.com/spreadsheets/d/1_LfCQsMJY-7Jd2-Gf6edboZnwFJMC_rmSpasdfa4/" className='mb-5' ref={sheetId}/>
           <Button type="submit" onClick={handleSubmit}>Guardar</Button>
         </div>
     )
