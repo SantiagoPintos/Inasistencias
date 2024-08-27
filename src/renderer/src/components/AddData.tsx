5b6f45b61c59326fa75f7528253d33bd54f423ff
Santiago Pintos <spintos@outlook.es>
2024-08-27 17:33:12 -0300

    refactor: Update AddData component to handle loading state and progress bar

--- src/renderer/src/components/AddData.tsx
+++ src/renderer/src/components/AddData.tsx
@@ -1,23 +1,37 @@
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 import { Button } from './ui/Button'
 import { Input } from './ui/Input'
 import { Label } from './ui/Label'
 import { AlertCircle } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "./ui/Alert"
+import { Progress } from './ui/Progress'
 
 const AddData = () => {
-    const token = useRef<HTMLInputElement>(null)
-    const sheetId = useRef<HTMLInputElement>(null)
+    const [token, setToken] = useState<string>('')
+    const [sheetId, setSheetId] = useState<string>('')
     const [showAlert, setShowAlert] = useState(false)
+    const [progress, setProgress] = useState(0)
+    const [loading, setLoading] = useState(false)
     
     const handleSubmit = async () => {
-      if(token.current?.value !== '' && sheetId.current?.value !== '' && sheetId.current?.value!==undefined && token.current?.value!==undefined){
-        await window.api.sendData(token.current!.value, sheetId.current?.value)
-        token.current!.value = ''
-        sheetId.current!.value = ''
-      } else {
-        setShowAlert(true)
-      }
+      setShowAlert(false)
+      setLoading(true)
+      setProgress(20)
+
+      setTimeout(async () => {
+        if(sheetId && token){
+          setProgress(50)
+          await window.api.sendData(token, sheetId)
+          setProgress(100)
+          setLoading(false)
+        } else {
+          setProgress(100)
+          setLoading(false)
+          setShowAlert(true)
+        }
+        setSheetId('')
+        setToken('')
+      }, 500); // Retardo para simular la carga
     }
 
     return (
@@ -29,11 +43,17 @@ const AddData = () => {
               <AlertDescription>Datos incorrectos</AlertDescription>
             </Alert>
           )}
-          <Label htmlFor="token">API Key</Label>
-          <Input type="text" id="token" placeholder="API Key" className='mb-5' ref={token}/>
-          <Label htmlFor="sheet" >URL de Google Sheets</Label>
-          <Input type="text" id="sheet" placeholder="https://docs.google.com/spreadsheets/d/1_LfCQsMJY-7Jd2-Gf6edboZnwFJMC_rmSpasdfa4/" className='mb-5' ref={sheetId}/>
-          <Button type="submit" onClick={handleSubmit}>Guardar</Button>
+          {loading ? (
+            <Progress value={progress} className="mb-5" />
+          ) : (
+            <>
+              <Label htmlFor="token">API Key</Label>
+              <Input type='text' id="token" placeholder="API Key" className='mb-5' value={token} onChange={(e)=>setToken(e.target.value)}/>
+              <Label htmlFor="sheet" >URL de Google Sheets</Label>
+              <Input type='text' id="sheet" placeholder="https://docs.google.com/spreadsheets/d/1_LfCQsMJY-7Jd2-Gf6edboZnwFJMC_rmSpasdfa4/" className='mb-5' value={sheetId} onChange={(e)=>setSheetId(e.target.value)}/>
+              <Button type="submit" onClick={handleSubmit}>Guardar</Button>
+            </>
+            )}
         </div>
     )
 }
