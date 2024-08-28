88c6ae48ff7bdd7142064cab1b00efedf41bca75
Santiago Pintos <spintos@outlook.es>
2024-08-28 18:21:20 -0300

    refactor: Update App component to include loading spinner for initial data fetch

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react'
 import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Dashboard from './components/Dashboard'
 import AddData from './components/AddData'
+import { LoadingSpinner } from './components/ui/Loading-spinner';
 
 function App(): JSX.Element {
-  const [thereIsData, setThereIsData] = useState<boolean>(true)
+  const [thereIsData, setThereIsData] = useState<boolean | null>(null)
   const [dataFromDatabase, setDataFromDatabase] = useState<any>([])
 
   useEffect(() => {
@@ -13,11 +14,21 @@ function App(): JSX.Element {
       if(dataFromDb) {
         setThereIsData(true)
         setDataFromDatabase(dataFromDb)
+      } else {
+        setThereIsData(false)
       }
     }
 
     data()
   }, [])
+
+  if (thereIsData === null) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <LoadingSpinner />
+      </div>
+    )
+  }
 
   return (
     <Router>
