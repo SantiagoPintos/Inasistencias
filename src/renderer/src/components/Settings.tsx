e1439a963f3d7d173f00e13679ae45e58a9d0392
Santiago Pintos <spintos@outlook.es>
2024-08-29 16:27:11 -0300

    refactor: Update Settings page header text and description

--- src/renderer/src/components/Settings.tsx
+++ src/renderer/src/components/Settings.tsx
@@ -5,9 +5,9 @@ const Settings = () => {
     <>
         <div className="hidden space-y-6 p-10 pb-16 md:block">
             <div className="space-y-0.5">
-                <h2 className="text-2xl font-bold tracking-tight">Ajustes</h2>
+                <h2 className="text-2xl font-bold tracking-tight">Configuración</h2>
                 <p className="text-muted-foreground">
-                    Administre los ajustes de la aplicación.
+                    Administre las diferentes características de la aplicación.
                 </p>
             </div>
             <Separator className="my-6" />
