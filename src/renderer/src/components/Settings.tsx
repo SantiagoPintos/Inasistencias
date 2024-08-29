1565de26a7dd4e63b8cd0fe9403e0fa23f6383a2
Santiago Pintos <spintos@outlook.es>
2024-08-29 17:39:35 -0300

    refactor: Add logs size to Settings page

--- src/renderer/src/components/Settings.tsx
+++ src/renderer/src/components/Settings.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react'
 import { Separator } from './ui/Separator'
 
 const Settings = () => {
+  const [ logsSize, setLogsSize ] = useState<number | null>(null)
+  useEffect(() => {
+    const logsFileSize = async () => {
+      const size = await window.api.getLogsFileSize()
+      if(size) setLogsSize(size)
+      console.log(size)
+    }
+    logsFileSize()
+  }, [])
+
+
   return (
     <>
         <div className="hidden space-y-6 p-10 pb-16 md:block">
@@ -11,6 +23,12 @@ const Settings = () => {
                 </p>
             </div>
             <Separator className="my-6" />
+            <div className="space-y-0.5">
+                <h2 className="text-2xl font-bold tracking-tight">Logs</h2>
+                <p className="text-muted-foreground">
+                    Tamaño de los logs: {logsSize ? Math.round(logsSize / 1024) : 0} KB
+                </p>
+            </div>
         </div>
     </>
   )
