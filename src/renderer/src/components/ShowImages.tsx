d26aefbe217fed8904da9fa28573d8c3a8b809e4
Santiago Pintos <spintos@outlook.es>
2024-09-01 20:36:37 -0300

    feat: ShowImages: Add dialog to save images

--- src/renderer/src/components/ShowImages.tsx
+++ src/renderer/src/components/ShowImages.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 import { Button } from "./ui/Button"
 import {
   Dialog,
@@ -17,6 +18,25 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs"
 import { Input } from "./ui/Input"
 
 const ShowImages = () => {
+  const url = useRef<HTMLInputElement>(null)
+  const filePath = useRef<HTMLInputElement>(null)
+
+  const handleSave = async (activeTab: string) => {
+    if(activeTab !== "url" && activeTab !== "file") return
+
+    if(activeTab === 'url') {
+      if(url.current?.value) {
+        window.api.sendImgUrl(url.current.value)
+      }
+    } else {
+      if(filePath.current?.files) {
+        const file =filePath.current.files[0]
+        //5MB
+        if(!file.type.startsWith('image/') || (file.size > 5242880)) return
+        await window.api.sendImgUrl(file.path)
+      }
+    }
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -41,11 +61,11 @@ const ShowImages = () => {
                 </CardHeader>
                 <CardContent className="space-y-2">
                   <div className="space-y-1">
-                    <Input id="username" defaultValue="" />
+                    <Input id="username" defaultValue="" ref={url} />
                   </div>
                 </CardContent>
                 <CardFooter>
-                  <Button>Guardar</Button>
+                  <Button onClick={()=>handleSave("url")}>Guardar</Button>
                 </CardFooter>
               </Card>
             </TabsContent>
@@ -58,11 +78,11 @@ const ShowImages = () => {
                 </CardHeader>
                 <CardContent className="space-y-2">
                   <div className="space-y-1">
-                    <Input id="username" type="file" defaultValue="" />
+                    <Input id="username" type="file" defaultValue="" ref={filePath}/>
                   </div>
                 </CardContent>
                 <CardFooter>
-                  <Button>Guardar</Button>
+                  <Button onClick={()=>handleSave("file")}>Guardar</Button>
                 </CardFooter>
               </Card>
 
