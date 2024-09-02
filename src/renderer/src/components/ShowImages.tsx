3773d0c4110143f21a6ae37e24ca9a2f5005aa0d
Santiago Pintos <spintos@outlook.es>
2024-09-01 21:18:22 -0300

    feat: Add error handling for saving images in ShowImages component

--- src/renderer/src/components/ShowImages.tsx
+++ src/renderer/src/components/ShowImages.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { Button } from "./ui/Button"
 import {
   Dialog,
@@ -16,27 +16,68 @@ import {
 } from "./ui/Card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/Tabs"
 import { Input } from "./ui/Input"
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "./ui/Alert"
+import { Check, AlertCircle } from "lucide-react"
 
 const ShowImages = () => {
   const url = useRef<HTMLInputElement>(null)
   const filePath = useRef<HTMLInputElement>(null)
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false)
+  const [showErrorAlert, setShowErrorAlert] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
 
   const handleSave = async (activeTab: string) => {
-    if(activeTab !== "url" && activeTab !== "file") return
-
-    if(activeTab === 'url') {
-      if(url.current?.value) {
-        window.api.sendImgUrl(url.current.value)
-      }
-    } else {
-      if(filePath.current?.files) {
-        const file =filePath.current.files[0]
-        //5MB
-        if(!file.type.startsWith('image/') || (file.size > 5242880)) return
-        await window.api.sendImgUrl(file.path)
-      }
+    if (activeTab !== "url" && activeTab !== "file") {
+      handleErrorAlert();
+      return;
     }
+  
+    console.log(activeTab)
+    try {
+      if (activeTab === "url") {
+        console.log('entrando a url')
+        if (!url.current?.value) {
+          setErrorMessage("La URL no es válida");
+          throw new Error("URL is required");
+        }
+        await window.api.sendImgUrl(url.current.value);
+      } else {
+        if (filePath.current?.files?.length !== 1) {
+          setErrorMessage("El archivo no es válido");
+          throw new Error("File is required");
+        }
+        const file = filePath.current.files[0];
+        //5MB
+        if (!file.type.startsWith("image/") || file.size >= 5242880) {
+          setErrorMessage("Tipo de archivo no válido o tamaño excedido");
+          throw new Error("Invalid file type or size");
+        }
+        await window.api.sendImgUrl(file.path);
+      }
+      handleSuccessAlert();
+    } catch (error) {
+      handleErrorAlert();
+    }
+  };
+
+  const handleSuccessAlert = () => {
+    setShowSuccessAlert(true)
+    setTimeout(() => {
+      setShowSuccessAlert(false)
+    }, 3000)
   }
+
+  const handleErrorAlert = () => {
+    setShowErrorAlert(true)
+    setTimeout(() => {
+      setShowErrorAlert(false)
+    }, 3000)
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -88,6 +129,24 @@ const ShowImages = () => {
 
             </TabsContent>
           </Tabs>
+          {showSuccessAlert && (
+            <Alert>
+              <Check className="h-4 w-4" />
+              <AlertTitle>Guardado!</AlertTitle>
+              <AlertDescription>
+                La imagen se guardó correctamente.
+              </AlertDescription>
+            </Alert>
+          )}
+          {showErrorAlert && (
+            <Alert variant="destructive">
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>
+                {errorMessage}.
+              </AlertDescription>
+            </Alert>
+          )}
         </div>
       </DialogContent>
     </Dialog>
