703f31a555962fd181d2f545e96fd3cfe20a5db8
Santiago Pintos <spintos@outlook.es>
2024-09-06 14:53:50 -0300

    refactor: Update types in env.d.ts for window.api.getData()

--- src/renderer/src/env.d.ts
+++ src/renderer/src/env.d.ts
@@ -1,18 +1,24 @@
 /// <reference types="vite/client" />
 
 declare global {
-    interface Window {
-      api: {
-        sendData: (key: string, url: string, sheetName: string) => Promise<void>;
-        getData: () => Promise<Object>;
-        openSettings: (callback: () => void) => void;
-        getLogsFileSize: () => Promise<number | null>;
-        clearLogs: () => Promise<void>;
-        sendImgUrl: (url: string) => Promise<void>;
-        getImgUrl: () => Promise<string | null>;
-        deleteImage: () => Promise<boolean>;
-      };
-    }
+  interface Window {
+    api: {
+      sendData: (key: string, url: string, sheetName: string) => Promise<void>;
+      getData: () => Promise<DataFromApi>;
+      openSettings: (callback: () => void) => void;
+      getLogsFileSize: () => Promise<number | null>;
+      clearLogs: () => Promise<void>;
+      sendImgUrl: (url: string) => Promise<void>;
+      getImgUrl: () => Promise<string | null>;
+      deleteImage: () => Promise<boolean>;
+    };
+  }
+  
+  interface DataFromApi {
+    range: string;
+    majorDimension: string;
+    values: string[][];
+  }
 }
 
 export {}
