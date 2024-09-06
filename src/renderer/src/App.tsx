115b2a5f600a8af24c0815e23c63dcb99f5d5cd1
Santiago Pintos <spintos@outlook.es>
2024-09-06 15:02:24 -0300

    refactor: Update App component to remove logic that was moved to Home

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,49 +1,24 @@
-import { useState, useEffect } from 'react'
-import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Dashboard from './components/Dashboard'
 import AddData from './components/AddData'
 import Settings from './components/Settings';
-import { LoadingSpinner } from './components/ui/Loading-spinner';
+import Home from './components/Home'
 
 function App(): JSX.Element {
   const navigate = useNavigate()
-  const [thereIsData, setThereIsData] = useState<boolean | null>(null)
-  const [dataFromDatabase, setDataFromDatabase] = useState<any>([])
 
   useEffect(() => {
     window.api.openSettings(() => {
       navigate('/settings')
     })
 
-    fetchData()
   }, [])
-
-
-  const fetchData = async () => {
-    const dataFromDb = await window.api.getData()
-    if(dataFromDb) {
-      setThereIsData(true)
-      setDataFromDatabase(dataFromDb)
-    } else {
-      setThereIsData(false)
-    }
-  }
-
-  if (thereIsData === null) {
-    return (
-      <div className="flex h-screen justify-center items-center">
-        <LoadingSpinner />
-      </div>
-    )
-  }
 
   return (
       <Routes>
-        <Route 
-          path="/" 
-          element={thereIsData ? <Navigate to="/dashboard" /> : <Navigate to="/add-data" />} 
-        />
-        <Route path="/dashboard" element={<Dashboard data={dataFromDatabase} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard data={undefined} />} />
         <Route path="/add-data" element={<AddData />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
