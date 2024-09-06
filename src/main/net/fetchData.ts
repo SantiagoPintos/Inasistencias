64856ad605d292e72f84f46b12fb39c4d50d4064
Santiago Pintos <spintos@outlook.es>
2024-09-06 14:38:33 -0300

    refactor: Refactor fetchData to use async/await for fetching data from Google Sheets API

--- src/main/net/fetchData.ts
+++ src/main/net/fetchData.ts
@@ -1,33 +1,13 @@
-import Logger from "../logger/logger"
-
 type dataFromDb = {
     token: string, 
     sheetId: string, 
     sheetName: string 
 }
 
-const logger = new Logger()
-export function fetchData(data: dataFromDb) {
+export async function fetchData(data: dataFromDb) {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${data.sheetId}/values/${data.sheetName}?key=${data.token}`
-    fetch(url,{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        } 
-    })
-    .then(response => {
-        if(response.ok) {
-            return response.json()
-        } else {
-            throw new Error(response.statusText)
-        }
-    })
-    .then(data => {
-        return data
-    })
-    .catch(err => {
-        logger.error('Error fetching the data')
-        console.log(err)
-        return null
-    })    
+    const response = await fetch(url)
+    if (!response.ok) throw new Error('Something went wrong') 
+    const json = await response.json()
+    return json  
 }
