5145916107444df51053c7a5fb9ee72262ec8e75
Santiago Pintos <spintos@outlook.es>
2024-09-06 16:34:40 -0300

    refactor: Add dataUpdate method to the API interface

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -3,6 +3,7 @@ import { contextBridge, ipcRenderer } from 'electron'
 interface Api {
   sendData: (token: string, url: string, sheetName: string) => void;
   getData: () => Promise<any>;
+  dataUpdate: (callback: (data: any) => void) => void;
   openSettings: (callback: () => void) => void;
   getLogsFileSize: () => Promise<number|null>;
   clearLogs: () => Promise<void>;
@@ -16,6 +17,7 @@ const api: Api = {
     ipcRenderer.send('send-data', token, url, sheetName);
   },
   getData: () => ipcRenderer.invoke('get-data'),
+  dataUpdate: (callback) => ipcRenderer.on('data-update', (_event, data) => callback(data)),
   openSettings: (callback) => ipcRenderer.on('open-settings', (_event) => callback()),
   getLogsFileSize: () => ipcRenderer.invoke('get-logs-file-size'),
   clearLogs: () => ipcRenderer.invoke('delete-logs'),
