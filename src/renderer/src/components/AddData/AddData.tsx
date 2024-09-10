508a8a8e9fc081d069ee4c2aa5e132052d3721b8
Santiago Pintos <spintos@outlook.es>
2024-09-10 14:50:50 -0300

    Refactor: Update import path for SelectSheetPage component in AddData.tsx

--- src/renderer/src/components/AddData/AddData.tsx
+++ src/renderer/src/components/AddData/AddData.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AddUrl } from './AddUrl'
-import SelectSheetPage from '../SelectSheetPage'
+import SelectSheetPage from './SelectSheetPage'
 import { Progress } from '../ui/Progress'
 import { Alert, AlertDescription, AlertTitle } from "../ui/Alert"
 import { Check } from 'lucide-react'
