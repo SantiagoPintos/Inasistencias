b08f158fb6223bfd294047b700eef6968bb556da
Santiago Pintos <spintos@outlook.es>
2024-09-10 13:38:42 -0300

    refactor: add logging in fetchData

--- src/main/net/fetchData.ts
+++ src/main/net/fetchData.ts
@@ -1,5 +1,7 @@
 import { databaseConnector } from './../dbManager/dbConnection' 
 import { getTokenAndSheetName } from './../dbManager/dbOperator'
+import Logger from '../logger/logger';
+const logger = new Logger();
 
 interface DataFromApi {
     range: string;
@@ -11,10 +13,14 @@ export async function fetchData(): Promise<DataFromApi|null> {
     const db = databaseConnector()
     const dataFromDb = await getTokenAndSheetName(db)
     if(dataFromDb.token === undefined || dataFromDb.token === null) {
+        logger.error('Token is not set')
         return null
     }
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${dataFromDb.sheetId}/values/${dataFromDb.sheetName}?key=${dataFromDb.token}`
     const response = await fetch(url)
-    if (!response.ok) throw new Error('Something went wrong') 
+    if (!response.ok) {
+        logger.error('Failed to fetch data from google sheets') 
+        throw new Error('Something went wrong')
+    } 
     return await response.json()
 }
