4edc5474b2d4077acf9ff0b0941af00670f22ec6
Santiago Pintos <spintos@outlook.es>
2024-09-10 14:27:51 -0300

    Refactor: Remove unused Redux slices

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { Provider } from 'react-redux'
 import { HashRouter as Router } from 'react-router-dom'
-import { store } from './store/store'
 import './styles/global.css'
 import '@fontsource/geist-sans'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>
   </React.StrictMode>
 )
