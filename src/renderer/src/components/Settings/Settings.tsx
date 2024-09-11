4c1b0bb2e92b79374a56c5b3d83663e73a775c59
Santiago Pintos <spintos@outlook.es>
2024-09-11 16:36:26 -0300

    Refactor: Update import paths for Settings component

--- src/renderer/src/components/Settings/Settings.tsx
+++ src/renderer/src/components/Settings/Settings.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Separator } from '../ui/Separator'
-import { Button } from '../ui/Button'
+import { Separator } from '@renderer/components/ui/Separator'
+import { Button } from '@renderer/components/ui/Button'
 
 const Settings = () => {
   const navigate = useNavigate()
