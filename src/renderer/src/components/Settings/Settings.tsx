d4b9cb59cca8c2da889f75db054e6b5808803cc4
Santiago Pintos <spintos@outlook.es>
2024-09-13 14:02:35 -0300

    feat: add section to delete all data in Settings component

--- src/renderer/src/components/Settings/Settings.tsx
+++ src/renderer/src/components/Settings/Settings.tsx
@@ -2,10 +2,32 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Separator } from '@renderer/components/ui/Separator'
 import { Button } from '@renderer/components/ui/Button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@renderer/components/ui/Alert-dialog"
+import {
+  Alert,
+  AlertDescription,
+  AlertTitle,
+} from "@renderer/components/ui/Alert"
+import { Check, AlertCircle } from 'lucide-react'
 
 const Settings = () => {
   const navigate = useNavigate()
   const [ logsSize, setLogsSize ] = useState<number | null>(null)
+  const [ showConfirmation, setShowConfirmation ] = useState(false)
+  const [ alertVariant, setAlertVariant ] = useState<'default' | 'destructive'>('default')
+  const [ alertTitle, setAlertTitle ] = useState<'Error'|'Correcto'>('Error')
+  const [ alertMessage, setAlertMessage ] = useState<string>('')
+  const [ alertIcon, setAlertIcon] = useState<React.ReactNode>(<Check />)
 
   useEffect(() => {
     const logsFileSize = async () => {
@@ -25,9 +47,37 @@ const Settings = () => {
     navigate('/')
   }
 
+  const handleDataDeletion = async () => {
+    const result = await window.api.deleteAllData()
+    setShowConfirmation(true)
+    if(result){
+      setAlertVariant('default')
+      setAlertTitle('Correcto')
+      setAlertIcon(<Check />)
+      setAlertMessage('Datos eliminados correctamente')
+    } else {
+      setAlertVariant('destructive')
+      setAlertTitle('Error')
+      setAlertIcon(<AlertCircle />)
+      setAlertMessage('Error al eliminar los datos')
+    }
+    setTimeout(() => {
+      setShowConfirmation(false)
+    }, 2000)
+  }
+
   return (
     <>
         <div className="hidden space-y-6 p-10 pb-16 md:block">
+        { showConfirmation &&
+          <Alert variant={alertVariant}>
+            {alertIcon}
+            <AlertTitle>{alertTitle}</AlertTitle>
+            <AlertDescription>
+              {alertMessage}
+            </AlertDescription>
+          </Alert>
+        }
             <div className="space-y-0.5">
                 <h2 className="text-2xl font-bold tracking-tight">Configuración</h2>
                 <p className="text-muted-foreground">
@@ -43,6 +93,28 @@ const Settings = () => {
                 <Button
                     onClick={handleDeleteClick}
                 > Eliminar registros </Button>
+            </div>
+            <Separator className="my-6" />
+            <div className="space-y-0.5">
+                <h2 className="text-2xl font-bold tracking-tight">Borrar datos</h2>
+                <p className="text-muted-foreground mb-2">Esta acción reiniciará la aplicación a su estado inicial</p>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="destructive">Eliminar datos</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Está seguro?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Esta acción no se puede deshacer. ¿Está seguro de que desea continuar?
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleDataDeletion}>Continuar</AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
             <Separator className="my-6" />
             <div className="space-y-0.5">
