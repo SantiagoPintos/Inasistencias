21e4946058340e0c46f3e202861a41bba948ab87
Santiago Pintos <spintos@outlook.es>
2024-09-18 08:31:28 -0300

    refactor: update Dashboard component to use ContextMenu component

--- src/renderer/src/components/Dashboard/Dashboard.tsx
+++ src/renderer/src/components/Dashboard/Dashboard.tsx
@@ -1,26 +1,65 @@
+import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import ShowData from './showData'
 import ShowImages from './ImageManager'
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from './../ui/ResizablePanel'
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@renderer/components/ui/ContextMenu"
 
 const Dashboard = () => {
   const location = useLocation()
   const data = location.state
+  const [panelVisible, setPanelVisible] = useState<boolean>(true)
+  const [panelTableSize, setPanelTableSize] = useState<number>(65)
+  const [panelImageSize, setPanelImageSize] = useState<number>(35)
+  const [btnText, setBtnText] = useState<'Ocultar panel de imágenes' | 'Mostrar panel de imágenes'>('Ocultar panel de imágenes')
+
+  const handlePanelVisibility = () => {
+    if (panelVisible) {
+      setPanelTableSize(100)
+      setPanelImageSize(0)
+      setBtnText('Mostrar panel de imágenes')
+    } else {
+      setPanelTableSize(65)
+      setPanelImageSize(35)
+      setBtnText('Ocultar panel de imágenes')
+    }
+    setPanelVisible(!panelVisible)
+  }
 
   return (
-    <ResizablePanelGroup direction="horizontal" className="max-w max-h">
-      <ResizablePanel defaultSize={65}>
-        <div className="">
-          <ShowData data={data} />
-        </div>
-      </ResizablePanel>
-      <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={35}>
-        <div className="h-screen flex items-center justify-center">
-          <ShowImages />
-        </div>
-      </ResizablePanel>
-    </ResizablePanelGroup>
+    <ContextMenu>
+      <ContextMenuTrigger> 
+        <ResizablePanelGroup direction="horizontal" className="max-w max-h">
+            <ResizablePanel 
+              key={panelTableSize}
+              defaultSize={panelTableSize} 
+              collapsible={true}>
+              <div>
+                <ShowData data={data} />
+              </div>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel
+              key={panelImageSize} 
+              defaultSize={panelImageSize} 
+              collapsible={true}>
+              <div className="h-screen flex items-center justify-center">
+                <ShowImages />
+              </div>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem onClick={handlePanelVisibility}>{btnText}</ContextMenuItem>
+        <ContextMenuItem>Ajustes</ContextMenuItem>
+        <ContextMenuItem>Reiniciar la aplicación</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
 
