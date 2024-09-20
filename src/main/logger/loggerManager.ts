a436bbed0fc696df70bee1ea81dcb84d046f7d52
Santiago Pintos <spintos@outlook.es>
2024-09-20 20:22:37 -0300

    refactor: update loggerManager to clear logs

--- src/main/logger/loggerManager.ts
+++ src/main/logger/loggerManager.ts
@@ -1,6 +1,9 @@
 import { app } from 'electron'
 import fs, { Stats } from 'fs'
 import path from 'path'
+import Logger from './logger'
+
+const logger = new Logger()
 
 export async function getLogsFileSize(): Promise<number> {
   const directoryPath: string = path.join(app.getPath('userData'), 'Logs')
@@ -28,16 +31,23 @@ function getFileSize(file: string): Promise<number> {
   })
 }
 
-export async function clearLogs(): Promise<void> {
-  const directoryPath: string = path.join(app.getPath('userData'), 'Logs')
-  const files: string[] = await fs.promises.readdir(directoryPath)
+export async function clearLogs(): Promise<boolean> {
+  try{
+    const directoryPath: string = path.join(app.getPath('userData'), 'Logs')
+    const files: string[] = await fs.promises.readdir(directoryPath)
+  
+    for (const file of files) {
+      const filePath: string = path.join(directoryPath, file)
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(err)
+        }
+      })
+    }
 
-  for (const file of files) {
-    const filePath: string = path.join(directoryPath, file)
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error(err)
-      }
-    })
+    return true
+  } catch (err) {
+    logger.error('Error deleting the logs: ' + (err as Error).message)
+    return false
   }
 }
